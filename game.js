2736c70b7ffde5cf7a162d3052974518dd867276
Stephan Allen <sallen202009@gmail.com>
2018-09-16 02:47:48 -0400

    finished the logic to the game

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
+// trivia game global vars and object
+let numAnswerCorrect = 0;
+let numAnswerWrong = 0;
 let questionNum = 0;
 let time = 30;
-var questionArr = [{
+let isAnswer;
+let myAnswer = 0;
+let intervalId;
+const questionArr = [{
     question: "what does the cat say?",
     answer: ['moooo','baaaaa','meow','roof'],
     correctNum: [0,0,1,0]
@@ -9,18 +15,20 @@ var questionArr = [{
     question: "what does the dog say?",
     answer: ['moooo','baaaaa','meow','roof'],
     correctNum: [0,0,0,1]   
+},
+{
+    question: "what is 2 + 2",
+    answer: ['4','7','10','3'],
+    correctNum: [1,0,0,0]   
 }];
-let isAnswer;
-let myAnswer = 0;
-let intervalId
-let button
-
+// trivia game logic
 function setQuestion(arr){
     let question = $('.question');
+    $('.appendHere').show();
     question.text(arr[questionNum].question);
     for(var i =0; i < arr[questionNum].answer.length; i++){
         let answer = $('<button>');
-        answer.addClass("buttonBlock center")
+        answer.addClass("buttonBlock center remove")
         answer.attr('correct', arr[questionNum].correctNum[i]);
         $('.appendHere').append(answer.text(arr[questionNum].answer[i]));
     }
@@ -39,18 +47,17 @@ function timerCountDown(){
         time--;
         $('.timer').text(time);
         if(time === 0){
+            numAnswerWrong++
+            let isFinished = gameFinished();
             $('.question').text('Out of Time!');
-            getAnswer();
             $('.appendHere').hide();
             clearInterval(intervalId);
-            setTimeout(function(){
-                nextQuestion()
-            }, 5000);
+            done(isFinished);
         }
     }, 1000)
 }
-
 function nextQuestion(){
+    // change back later to 30
         time = 30;
         questionNum++
         $('.appendHere').show();
@@ -59,30 +66,28 @@ function nextQuestion(){
         setQuestion(questionArr);
         timerCountDown();
 }
-
 function answerCheck(){
     $('.appendHere').on('click','button', function(){
         button = $(this);
         myAnswer = parseInt(button.attr('correct'));
         if(myAnswer === 1){
+            numAnswerCorrect++
             $('.appendHere').hide();
+            let isFinished = gameFinished();
             clearInterval(intervalId);
             printRightOrWrong(myAnswer);
-            setTimeout(function(){
-                nextQuestion()
-            }, 5000);
+            done(isFinished);
         }else{
+            numAnswerWrong++
+            $('.question').text(numAnswerCorrect);
             $('.appendHere').hide();
-            clearInterval(intervalId);
-            getAnswer();
+            let isFinished = gameFinished();
             printRightOrWrong(myAnswer)
-            setTimeout(function(){
-                nextQuestion()
-            }, 5000);
+            clearInterval(intervalId);
+            done(isFinished);
         }
     })
 }
-
 function printRightOrWrong(answer){
     if(answer === 1){
         $('.question').text('Your Right!')
@@ -90,28 +95,73 @@ function printRightOrWrong(answer){
         $('.question').text('Sorry Wrong Answer!')
     }
 }
-
-function outOfTime(){
-    setInterval(function(){
-        if(parseInt($('.timer').text(0)) === 0){
-            $('.question').text('Out of Time!');
-            correctAnswer(button.text())
-            console.log(parseInt($('.timer').text(0)) === 0);
-        }
-    }, 500)
-}
 function correctAnswer(theAnswer){
     $('#correctAnswer').text('The correct answer was: ' + theAnswer).show();
 }
-
-
+function gameFinished(){
+    if(questionNum === questionArr.length -1){
+        getAnswer();
+        setTimeout(function(){
+            $('.question').hide();
+            let numRight = $('<p>');
+            numRight.addClass('text-center removeScoreBoard')
+            numRight.text('right: ' + numAnswerCorrect)
+            let numWrong = $('<p>');
+            numWrong.addClass('text-center removeScoreBoard')
+            numWrong.text('wrong: ' + numAnswerWrong)
+            $('.scoreBoard').append(numRight);
+            $('.scoreBoard').append(numWrong);
+            let playAgain = $('<button>');
+            playAgain.text('Play Again');
+            playAgain.addClass('center removeScoreBoard');
+            $('.scoreBoard').append($(playAgain));
+            $('#correctAnswer').hide();
+        },3000)
+        return true;
+    }else{
+        return false;
+    }
+}
+function playAgain(){
+    $('.scoreBoard').on('click', 'button', function(){
+        $('.question').show();
+        $('.removeScoreBoard').remove();
+        $('.remove').remove();
+        numAnswerCorrect = 0;
+        numAnswerWrong = 0;
+        time = 30;
+        questionNum = 0;
+        timerCountDown();
+        setQuestion(questionArr);
+    })
+}
+function done(done){
+    if(done === false){
+        getAnswer();
+        setTimeout(function(){
+            nextQuestion()
+        }, 3000);
+    }
+}
+function start(){
+    let startButton = $('<button>');
+    startButton.addClass('center removeStartButton startButton');
+    startButton.text('Start')
+    $('.appendHere').append(startButton);
+    $('.startButton').on('click',function(){
+        $('.countDown').show()
+        $('.removeStartButton').remove();
+        timerCountDown();
+        setQuestion(questionArr);
+        answerCheck();
+        playAgain();
+    })
+}
+// trivia game start
 $(document).ready(function(){
-    timerCountDown();
-    setQuestion(questionArr);
-    answerCheck();
+    $('.countDown').hide()
+    start();
 })
-
-    
 
 
 
