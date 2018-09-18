712cf1188371f8361c3f584e594430723b6542b5
Stephan Allen <sallen202009@gmail.com>
2018-09-18 00:50:38 -0400

    finshed styling and added final questions

--- game.js
+++ game.js
@@ -6,19 +6,59 @@ let time = 30;
 let isAnswer;
 let myAnswer = 0;
 let intervalId;
+let slideUp = {
+    distance: '1050%',
+    origin: 'bottom',
+    opacity: null
+};
 const questionArr = [{
-    question: "what does the cat say?",
-    answer: ['moooo','baaaaa','meow','roof'],
-    correctNum: [0,0,1,0]
+    question: "Who sang Express Yourself?",
+    answer: ['Run DMC','Sugar Hill Gang','The Fugees','NWA'],
+    correctNum: [0,0,0,1]
 },
 {
-    question: "what does the dog say?",
-    answer: ['moooo','baaaaa','meow','roof'],
+    question: "What was 2Pac's 1st album called?",
+    answer: ['Loyal To The Game','Until The End Of Time','The Lost Tapes','2Pacalypse Now'],
     correctNum: [0,0,0,1]   
 },
 {
-    question: "what is 2 + 2",
-    answer: ['4','7','10','3'],
+    question: "Phife Dawg, Q-Tip, and Ali Shaheed Muhammad were the members of what rap group?",
+    answer: ['De La Soul','A Tribe Called Quest','Arrested Development','Digital underground'],
+    correctNum: [0,1,0,0]   
+},
+{
+    question: "Complete these lyrics: 'Now in my younger days I used to sport a shag / When I went to school____________'",
+    answer: ['I always had lots of swag','I flunked all my exams','I carried lunch in a bag','I did not have a backpack'],
+    correctNum: [0,0,1,0]   
+},
+{
+    question: "What was the name of Fresh Prince's DJ?",
+    answer: ['DJ Duke of Earl','DJ Jazzy Jeff','DJ Funk Flexmaster','DJ King Coopa'],
+    correctNum: [0,1,0,0]   
+},
+{
+    question: "Which Nas song did Jay-Z sample for his song 'Dead Presidents'?",
+    answer: ['The World is Yours','One Mic','If I Ruled The World','Made U Look'],
+    correctNum: [1,0,0,0]   
+},
+{
+    question: "Which is the only A Tribe Called Quest album to feature Jarobi White as a member of the group?",
+    answer: ['The Low End Theory','Beats, Rhymes and Life',"People's Instinctive Travels and the Paths of Rhythm",'The Love Movement'],
+    correctNum: [0,0,1,0]   
+},
+{
+    question: "What was Outkast third studio album",
+    answer: ['Southernplayalisticadillacmuzik','ATLiens','Stankonia','Aquemini'],
+    correctNum: [0,0,0,1]   
+},
+{
+    question: "Dre began his career as a member of",
+    answer: ['NWA','Aftermath',"World Class Wreckin' Cru",'Interscope'],
+    correctNum: [0,0,1,0]   
+},
+{
+    question: "Hip Hop was created by..",
+    answer: ['DJ Kool Herc','Afrika Bambaataa','The Sugarhill Gang','RUN DMC'],
     correctNum: [1,0,0,0]   
 }];
 // trivia game logic
@@ -28,9 +68,10 @@ function setQuestion(arr){
     question.text(arr[questionNum].question);
     for(var i =0; i < arr[questionNum].answer.length; i++){
         let answer = $('<button>');
-        answer.addClass("buttonBlock center remove")
+        answer.addClass("buttonBlock center remove load-hidden")
         answer.attr('correct', arr[questionNum].correctNum[i]);
         $('.appendHere').append(answer.text(arr[questionNum].answer[i]));
+        ScrollReveal().reveal('.buttonBlock', slideUp);
     }
 }
 function getAnswer(){
@@ -144,6 +185,8 @@ function done(done){
 }
 function start(){
     $('.startButton').on('click',function(){
+        var audio = new Audio('/audio/Knxwledge - KNX anthologhy TAPE 1.mp3');
+        audio.play();
         $('.countDown').show()
         $('.removeStartButton').remove();
         timerCountDown();
